bd975abd76a6ee84233057fe8cff07580536f944
Kevin Perriat <kevin.perriat@hotmail.fr>
2025-03-07 11:19:43 +0100

    tech: alert component

---
+++ src/stories/alert.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CircleAlert, CircleCheck, InfoIcon, TriangleAlert } from "lucide-react";
+
+interface ALertStoryArgs {
+  variant: "info" | "success" | "warning" | "destructive";
+  title: string;
+  description: string;
+  withIcon: boolean;
+}
+
+const meta = {
+  title: "Components/Alert",
+  component: Alert,
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+      },
+      options: ["info", "success", "warning", "destructive"],
+    },
+  },
+  args: {
+    variant: "info",
+  },
+} satisfies Meta<typeof Alert>;
+
+export default meta;
+
+type Story = StoryObj<ALertStoryArgs>;
+
+export const Default: Story = {
+  args: {
+    variant: "info",
+    title: "Heads up!",
+    description:
+      "You can add components and dependencies to your app using the cli.",
+    withIcon: true,
+  },
+  render: function Render(args) {
+    let Icon = InfoIcon;
+
+    switch (args.variant) {
+      case "info":
+        Icon = InfoIcon;
+        break;
+      case "success":
+        Icon = CircleCheck;
+        break;
+      case "warning":
+        Icon = TriangleAlert;
+        break;
+      case "destructive":
+        Icon = CircleAlert;
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <Alert variant={args.variant}>
+        {args.withIcon && <Icon className="size-4" />}
+        {args.title && <AlertTitle>{args.title}</AlertTitle>}
+        <AlertDescription>{args.description}</AlertDescription>
+      </Alert>
+    );
+  },
+};
