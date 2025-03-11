a79b2a423ec51e153133c0457f9604e45c161d48
Clement Grihon <gclems@gmail.com>
2025-03-11 15:02:24 +0100

    tech: exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,61 @@
 export { cn } from "@/lib/utils";
 
-export { Button } from "@/components/ui/button";
+export { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+export { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 export { Badge } from "@/components/ui/badge";
+export { Button } from "@/components/ui/button";
+export { Calendar } from "@/components/ui/calendar";
+export { Checkbox } from "@/components/ui/checkbox";
+export {
+  DatePicker,
+  DatePickerTrigger,
+} from "@/components/compound/datePicker";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+export {
+  DialogWrapper,
+  DialogWrapperTrigger,
+  DialogWrapperContent,
+  DialogWrapperHeader,
+  DialogWrapperBody,
+  DialogWrapperFooter,
+  DialogWrapperTitle,
+  DialogWrapperDescription,
+  DialogWrapperClose,
+} from "@/components/compound/dialogWrapper";
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+} from "@/components/ui/popover";
+export { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+export { Select } from "@/components/ui/select";
+export {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetBody,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
